433668ad7f606715c3b494577b8b20eb43bdce4f
vijay manthu <mahi.manthu10@gmail.com>
2021-04-26 20:43:36 +0530

    final_update_sql

--- sql/hmusic.sql
+++ sql/hmusic.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2021 at 08:12 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Generation Time: Apr 26, 2021 at 05:10 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -51,7 +51,8 @@ CREATE TABLE `artist` (
   `artist_name` varchar(45) DEFAULT NULL,
   `artist_biography` text DEFAULT NULL,
   `artist_details` varchar(45) DEFAULT NULL,
-  `artist_photo` text NOT NULL
+  `artist_photo` text NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -70,17 +71,6 @@ CREATE TABLE `downloads` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genre`
---
-
-CREATE TABLE `genre` (
-  `id` int(11) NOT NULL,
-  `type` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `karoke`
 --
 
@@ -89,7 +79,8 @@ CREATE TABLE `karoke` (
   `name` varchar(30) NOT NULL,
   `photo` varchar(1000) NOT NULL,
   `music` varchar(1000) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `verify` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -115,11 +106,11 @@ CREATE TABLE `songs` (
   `song_mp3` varchar(1000) DEFAULT NULL,
   `song_photo` text DEFAULT NULL,
   `song_date` text DEFAULT NULL,
-  `aritst_id` varchar(35) DEFAULT NULL,
+  `aritst_id` int(11) DEFAULT NULL,
   `song_name` varchar(225) NOT NULL,
   `verify` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `type_id` int(11) NOT NULL
+  `type_id` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -142,6 +133,15 @@ CREATE TABLE `users` (
   `block_status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `first_name`, `last_name`, `reg_date`, `last_seen`, `photo`, `verify`, `code`, `block_status`) VALUES
+(1, 'mahi.manthu10@gmail.com', '$2y$10$jnV6JQtL/Sww8G4xNUCs0.3szx3V7lsb079UNiLP.l4mz.xBMo0u6', 'MAHANTESH', NULL, '2021-03-24', '2021-04-26', '', 0, '', 0),
+(2, 'jobs.manthu10@gmail.com', '$2y$10$qs693J9MpUS1PDvR1SuUH.Yo1Q6ijgzCx7zxzocNk5pl.GQriZdNC', 'MAHANTESH', NULL, '2021-03-24', '2021-04-26', '', 0, '', 0),
+(3, 'vj@gmail.com', '$2y$10$RQem.J2oPmwsPO19ZMwIUuvXucPPuMJ//PTYxPrKR09fxs.LcqO0G', 'vj', NULL, '2021-03-24', '2021-03-24', '', 0, '', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -154,14 +154,6 @@ CREATE TABLE `views` (
   `song_id` int(11) DEFAULT NULL,
   `view_time` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `views`
---
-
-INSERT INTO `views` (`view_id`, `user_id`, `song_id`, `view_time`) VALUES
-(1, 1, 1, '1616303653'),
-(2, 2, 2, '1616393409');
 
 --
 -- Indexes for dumped tables
@@ -183,31 +175,32 @@ ALTER TABLE `artist`
 -- Indexes for table `downloads`
 --
 ALTER TABLE `downloads`
-  ADD PRIMARY KEY (`download_id`);
-
---
--- Indexes for table `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`download_id`),
+  ADD KEY `song_id` (`song_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `karoke`
 --
 ALTER TABLE `karoke`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user_id`);
 
 --
 -- Indexes for table `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `songs_id` (`songs_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `songs`
 --
 ALTER TABLE `songs`
-  ADD PRIMARY KEY (`song_id`);
+  ADD PRIMARY KEY (`song_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `aritst_id` (`aritst_id`);
 
 --
 -- Indexes for table `users`
@@ -219,7 +212,11 @@ ALTER TABLE `users`
 -- Indexes for table `views`
 --
 ALTER TABLE `views`
-  ADD PRIMARY KEY (`view_id`);
+  ADD PRIMARY KEY (`view_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `user_id_2` (`user_id`),
+  ADD KEY `song_id` (`song_id`),
+  ADD KEY `user_id_3` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -235,7 +232,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `artist`
 --
 ALTER TABLE `artist`
-  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `downloads`
@@ -244,40 +241,72 @@ ALTER TABLE `downloads`
   MODIFY `download_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `genre`
---
-ALTER TABLE `genre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `karoke`
 --
 ALTER TABLE `karoke`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `views`
 --
 ALTER TABLE `views`
   MODIFY `view_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `downloads`
+--
+ALTER TABLE `downloads`
+  ADD CONSTRAINT `ss_id` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
+  ADD CONSTRAINT `u_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `karoke`
+--
+ALTER TABLE `karoke`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `song_id` FOREIGN KEY (`songs_id`) REFERENCES `songs` (`song_id`),
+  ADD CONSTRAINT `user_idd` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `artist` FOREIGN KEY (`aritst_id`) REFERENCES `artist` (`artist_id`),
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `views`
+--
+ALTER TABLE `views`
+  ADD CONSTRAINT `s_id` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`),
+  ADD CONSTRAINT `uid` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
